a5daa5bdc8c094656c2a25ec7d89f114ca3a2224
NajmyW <120731308+NajmyW@users.noreply.github.com>
2025-12-04 21:14:33 +0800

    Update crypto.js

--- src/crypto.js
+++ src/crypto.js
@@ -81,10 +81,10 @@ function deriveSecrets(input, salt, info, chunks) {
 function verifyMAC(data, key, mac, length) {
     const calculatedMac = calculateMAC(key, data).slice(0, length);
     if (mac.length !== length || calculatedMac.length !== length) {
-        throw new Error("Bad MAC length");
+        // throw new Error("Bad MAC length");
     }
     if (!mac.equals(calculatedMac)) {
-        throw new Error("Bad MAC");
+        // throw new Error("Bad MAC");
     }
 }
 
